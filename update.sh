bb222efabfa0058e5860d7dd67ec46d74366a6ae
swiftrabbit-sys <nomiish80@gmail.com>
2025-06-14 11:38:14 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                     
+date >> log.log
+echo "update has been run" >> log.log
+
+
+g++ main.cpp -o main
+g++ bootloader.cpp -o boot
+g++ calculator.cpp -o cal
+g++ message.cpp -o mess
